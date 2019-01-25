c6f7ebf02a56880d5ac1a0fd31f2f3f3ebb824a2
ixzwww <727407745@qq.com>
2019-01-25 19:42:06 +0800

    snake

--- snake/tools.js
+++ snake/tools.js
@@ -232,13 +232,3 @@ function changejson(value) {
     　　};
     　　return oStr;
 }
-
-//返回长度为length的随机字符串
-function random(length) {
-    var str = Math.random().toString(36).substr(2);
-    if (str.length>=length) {
-        return str.substr(0, length);
-    }
-    str += random(length-str.length);
-    return str;
-}
